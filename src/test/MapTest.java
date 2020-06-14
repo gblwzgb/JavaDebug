8e6ee25ca9d1445783c2f0261a875607a38bf1b1
panshen <panshen@gegejia.com>
2020-06-15 00:22:11 +0800

    first commit

---
+++ src/test/MapTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+    }
+}
